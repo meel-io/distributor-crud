ceb8f4ad76f31478797c36d129c7a75a39d07005
meel-io <emiliodelatorrea@gmail.com>
2017-12-05 22:05:51 +0000

    Update worker methods

--- worker.js
+++ worker.js
@@ -1,29 +1,33 @@
 require('dotenv').config()
 const zmq = require('zmq')
-const schema = require('sb-schema')
+const { connect, Player } = require('sb-schema')
 
-const { DBHOST, DBPORT, DBNAME, DBUSERNAME, DBPASSWORD } = process.env
-schema.connect({
-  type: 'postgres',
-  username: DBUSERNAME,
-  password: DBPASSWORD,
-  host: DBHOST,
-  port: DBPORT,
-  name: DBNAME
-})
+const run = async () => {
+  const { DBHOST, DBPORT, DBNAME, DBUSERNAME, DBPASSWORD } = process.env
+  const connection = await connect({
+    type: 'postgres',
+    username: DBUSERNAME,
+    password: DBPASSWORD,
+    host: DBHOST,
+    port: DBPORT,
+    name: DBNAME
+  }).catch(error => {
+    throw error
+  })
 
-const getPlayerRepository = schema.getPlayerRepository()
-const fromDispatcher = zmq.socket('pull')
-const toSink = zmq.socket('push')
+  const getPlayerRepository = connection.getRepository(Player)
+  const fromDispatcher = zmq.socket('pull')
+  const toSink = zmq.socket('push')
 
-const dispatcherPort = process.env.DISPATCHER_PORT || 5016
-const sinkPort = process.env.SINK_PORT || 5017
+  const dispatcherPort = process.env.DISPATCHER_PORT || 5016
+  const sinkPort = process.env.SINK_PORT || 5017
 
-fromDispatcher.connect(`tcp://localhost:${dispatcherPort}`)
-toSink.connect(`tcp://localhost:${sinkPort}`)
+  fromDispatcher.connect(`tcp://localhost:${dispatcherPort}`)
+  toSink.connect(`tcp://localhost:${sinkPort}`)
 
-fromDispatcher.on('message', async buffer => {
-  const { firstname, lastname } = JSON.parse(buffer)
-  const user = await getPlayerRepository.create({ firstname, lastname })
-  toSink.send(`Inserted: ${user.id}`)
-})
+  fromDispatcher.on('message', async buffer => {
+    const { firstname, lastname } = JSON.parse(buffer)
+    const user = await getPlayerRepository.create({ firstname, lastname })
+    toSink.send(`Inserted: ${user.id}`)
+  })
+}
