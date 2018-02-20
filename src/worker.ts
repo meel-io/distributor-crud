1c193b1ea9fe13d2bf33cdc51c9bcbd0550829c5
e1000-io <emiliodelatorrea@gmail.com>
2018-02-20 22:42:18 +0000

    All moved to TS

--- src/worker.ts
+++ src/worker.ts
@@ -1,19 +1,19 @@
 const zmq = require('zmq')
 
-const run = async (rowMethod, dispatcherPort, sinkPort) => {
+const run = async (rowMethod: Function, dispatcherPort: number, sinkPort: number) => {
   const fromDispatcher = zmq.socket('pull')
   const toSink = zmq.socket('push')
 
   fromDispatcher.connect(`tcp://localhost:${dispatcherPort}`)
   toSink.connect(`tcp://localhost:${sinkPort}`)
 
-  fromDispatcher.on('message', async data => {
+  fromDispatcher.on('message', async (data: Object) => {
     const { rows } = JSON.parse(data.toString())
-    rows.map(async row => {
+    rows.map(async (row: any[]) => {
       await rowMethod(row)
       toSink.send(`row Processed`)
     })
   })
 }
 
-module.exports = { run }
+export { run }
