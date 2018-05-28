32f03938d560119466b4b444bac557e5875b78e5
e1000-io <emiliodelatorrea@gmail.com>
2018-05-28 22:46:29 +0100

    Fix coveralls

--- src/batch.test.ts
+++ src/batch.test.ts
@@ -4,12 +4,31 @@ import * as chai from 'chai'
 import * as mocha from 'mocha'
 
 const assert = chai.assert
+const BATCH_SIZE = 5
+
 describe('Batch clear method', () => {
-  const BATCH_SIZE = 5
   const batch = new Batch(BATCH_SIZE)
 
   it('should empty batch', () => {
+    batch.push(Buffer.from('hello world'))
     batch.clear()
     assert.isEmpty(batch.rows)
+  })
+})
+
+describe('Batch full method', () => {
+  const batch = new Batch(BATCH_SIZE)
+
+  it('should return false if not full', () => {
+    batch.push(Buffer.from("There's a starman waiting in the sky"))
+    assert.isFalse(batch.full())
+  })
+
+  it('should return true if full', () => {
+    batch.push(Buffer.from("He'd like to come and meet us"))
+    batch.push(Buffer.from("But he thinks he'd blow our minds"))
+    batch.push(Buffer.from("There's a starman waiting in the sky"))
+    batch.push(Buffer.from("He's told us not to blow it"))
+    assert.isTrue(batch.full())
   })
 })
